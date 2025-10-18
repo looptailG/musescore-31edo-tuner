b5aa7ccaa675173e26027da8f18a4dec6a2cda95
Alessandro Culatti <99362337+looptailG@users.noreply.github.com>
2025-10-18 18:09:56 +0200

    Added computing of enharmonic equivalent notes

--- source/libs/31EdoUtils.js
+++ source/libs/31EdoUtils.js
@@ -18,16 +18,55 @@
 
 const VERSION = "1.0.0";
 
-// Distance of each note in EDO steps from the letter C.
-const SCALE_STEPS = {
+// Distance of each note in EDO steps from the note C.
+const NOTES_STEPS = {
 	"C": 0,
 	"D": 5,
 	"E": 10,
 	"F": 13,
 	"G": 18,
 	"A": 23,
-	"B": 28,
+	"B": 28
 };
+
+// Size in EDO stpes of each accidental.
+const ACCIDENTALS_STPES = {
+	"bbb": -3,
+	"bb": -2,
+	"b": -1,
+	"": 0,
+	"#": 1,
+	"x": 2,
+	"#x": 3
+};
+
+// Map every EDO step to an array of every possible enharmonic spelling for that
+// note.  The array are ordered alphabetically.
+const ENHARMONIC_EQUIVALENTS = {};
+for (let i = 0; i < 31; i++)
+{
+	let enharmonicequivalents = [];
+	
+	for (const note of NOTES_STEPS.keys())
+	{
+		for (const accidentals of ACCIDENTALS_STPES.keys())
+		{
+			let edoSteps = NOTES_STEPS[note] + ACCIDENTALS_STPES[accidental];
+			edoSteps %= 31;
+			while (edoSteps < 0)
+			{
+				edoSteps += 31;
+			}
+			
+			if (i == edoSteps)
+			{
+				enharmonicequivalents.push(note + accidental);
+			}
+		}
+	}
+	
+	ENHARMONIC_EQUIVALENTS[i] = enharmonicequivalents;
+}
 
 /**
  * Return a Set containing every note that is enharmonically equivalent to the

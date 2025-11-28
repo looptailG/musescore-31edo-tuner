4251101d00fa738c7c5028df43c0f50ac0b8701b
Alessandro Culatti <99362337+looptailG@users.noreply.github.com>
2025-11-29 00:08:43 +0100

    Added readTsvFile()

--- source/SettingsIO.js
+++ source/SettingsIO.js
@@ -1,6 +1,6 @@
 /*
 	QML component for reading and writing configuration files.
-	Copyright (C) 2024 - 2025 Alessandro Culatti
+	Copyright (C) 2025 Alessandro Culatti
 
 	This program is free software: you can redistribute it and/or modify
 	it under the terms of the GNU General Public License as published by
@@ -17,3 +17,24 @@
 */
 
 const VERSION = "1.0.0";
+
+/**
+ * Read the input TSV file, and return its content as a dictionary, where the
+ * keys are the content of keyColumn, and the values the content of valueColumn.
+ */
+function readTsvFile(filePath, fileIO, keyColumn = 0, valueColumn = 1)
+{
+	let settings = {};
+	
+	let fileContent = fileIO.read().split("\n");
+	for (let i = 0; i < fileContent.length; i++)
+	{
+		if (fileContent[i])
+		{
+			let rowData = parseTsvRow(fileContent[i]);
+			settings[rowData[keyColumn]] = rowData[valueColumn];
+		}
+	}
+	
+	return settings;
+}

902a625a837e70e95dc41b0a1ddb2f7af9e7455f
Alessandro Culatti <99362337+looptailG@users.noreply.github.com>
2025-11-29 00:10:25 +0100

    Added utility functions

--- source/SettingsIO.js
+++ source/SettingsIO.js
@@ -38,3 +38,87 @@ function readTsvFile(filePath, fileIO, keyColumn = 0, valueColumn = 1)
 	
 	return settings;
 }
+
+/**
+ * Split the input string using the tab character, and replace the escaped
+ * characters.
+ */
+function parseTsvRow(s)
+{
+	s = s.split("\t");
+	// QML does not support lookbehind in regex, which would be necessary to
+	// properly unescape the characters, so we have to manually loop on the
+	// strings and check for escape characters.
+	for (let i = 0; i < s.length; i++)
+	{
+		let unescapedString = "";
+		let escapedString = s[i];
+		let j = 0;
+		while (j < escapedString.length)
+		{
+			let c = escapedString.charAt(j);
+			if (c == "\\")
+			{
+				let nextCharacter = escapedString.charAt(++j);
+				switch (nextCharacter)
+				{
+					case "\\":
+						unescapedString += "\\";
+						break;
+					
+					case "n":
+						unescapedString += "\n";
+						break;
+					
+					case "r":
+						unescapedString += "\r";
+						break;
+					
+					case "t":
+						unescapedString += "\t";
+						break;
+					
+					default:
+						throw "Invalid escape sequence: " + c + nextCharacter;
+				}
+			}
+			else
+			{
+				unescapedString += c;
+			}
+
+			j++;
+		}
+		s[i] = unescapedString;
+	}
+	return s;
+}
+
+/**
+ * Escape the necessary characters for a TSV file in the input string.
+ */
+function formatForTsv(s)
+{
+	s = s.replace(/\t/g, "\\t");
+	s = s.replace(/\\/g, "\\\\");
+	s = s.replace(/\n/g, "\\n");
+	s = s.replace(/\r/g, "\\r");
+	return s;
+}
+
+/**
+ * Remove the empty rows from the input string.  The resulting string will have
+ * a new line character at the end.
+ */
+function removeEmptyRows(s)
+{
+	s = s.split("\n");
+	for (let i = s.length - 1; i >= 0; i--)
+	{
+		if (s[i].trim() == "")
+		{
+			s.splice(i, 1);
+		}
+	}
+	return s.join("\n") + "\n";
+}

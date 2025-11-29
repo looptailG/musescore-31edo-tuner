673bc4025421c36937f8eb665d3c6671b7b927b9
Alessandro Culatti <99362337+looptailG@users.noreply.github.com>
2025-11-29 17:35:37 +0100

    Updated python script to work with multiple thumbnails

--- out/Generate31EDOPlugin.py
+++ out/Generate31EDOPlugin.py
@@ -1,18 +1,16 @@
 import os
+import glob
 import shutil
 import re
 
 
 PLUGIN_FOLDER_NAME = "31edo_tuner"
 SOURCE_FOLDER = "../source"
+THUMBNAILS_FOLDER_NAME = "thumbnails"
 LOGS_FOLDER = f"{PLUGIN_FOLDER_NAME}/logs"
 README_PATH = "../README.md"
 LICENSE_PATH = "../LICENSE"
-THUMBNAIL_PATH = "../thumbnail/31EdoThumbnail.png"
-FILES_TO_COPY = [
-    LICENSE_PATH,
-    THUMBNAIL_PATH,
-]
+FILES_TO_COPY = [LICENSE_PATH]
 
 
 def main():
@@ -21,8 +19,15 @@ def main():
         for file_path in FILES_TO_COPY:
             file_name = file_path[file_path.rindex("/") + 1:]
             shutil.copyfile(file_path, f"{PLUGIN_FOLDER_NAME}/{file_name}")
+        
         if not os.path.exists(LOGS_FOLDER):
             os.makedirs(LOGS_FOLDER)
+        
+        if not os.path.exists(f"{PLUGIN_FOLDER_NAME}/{THUMBNAILS_FOLDER_NAME}"):
+            os.makedirs(f"{PLUGIN_FOLDER_NAME}/{THUMBNAILS_FOLDER_NAME}")
+        for file_path in glob.glob(f"../{THUMBNAILS_FOLDER_NAME}/*.png"):
+            file_name = file_path[file_path.rindex("/") + 1:]
+            shutil.copyfile(file_path, f"{PLUGIN_FOLDER_NAME}/{file_name}")
 
         version_number = get_version_number()
         output_folder_name = f"{PLUGIN_FOLDER_NAME}_{version_number}"
